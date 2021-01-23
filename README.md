471d6fdcf3b042091798f2fbacf665cbba5db68c
Ryan <ryan@Ryans-iMac.local>
2021-01-23 15:40:03 -0800

    added js prompt file

---
+++ README.md
@@ -0,0 +1,43 @@
+
+
+   [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
+
+
+   # Title
+   Ryan
+
+   ## Table of Contents
+
+   
+
+
+   [Installation](#Installation)
+    <br>
+   [Usage](#Usage)
+   <br>
+   [Conbtribution](#Contribution)
+   <br>
+   [Testing](#Testing)
+
+
+  ## Installation
+  R
+
+  ## Usage Information
+  
+
+  ## Contribution Information
+  undefined
+
+  ## Testing Information
+  
+
+   ## Questions
+
+   Please feel free to reach out with any additional questions through my email or on github.
+   <br>
+   https://github.com/rjmurphy1922
+   <br>
+  undefined
+
+   
---
+++ node.js
@@ -0,0 +1,126 @@
+const inquirer = require("inquirer")
+const fs = require("fs")
+ userPrompt = () => {
+inquirer
+  .prompt([
+    
+    {
+  
+    type: "input",
+    name:"github",
+    message: "Please Enter Your Project Title"
+
+    },
+{
+
+    type: "input",
+    name: "Installation",
+    message:" Please Enter Installation Instructions",
+   
+
+},
+{
+    type: "input",
+    name: "Usage",
+    message:"Please Enter Usage Information",
+},
+{
+    type: "input",
+    name: "Conribution",
+    message:"Contribution Guidelines",
+
+},
+{
+    type: "input",
+    name: "Test",
+    message:"Please Enter Testing Information ",
+},
+
+{
+  type: "list",
+  name: "License",
+  message:"Please select a License ",
+  choices:[
+    "MIT",
+    "Apache license 2.0",
+    "Mozilla Public License 2.0"
+]
+},
+
+{
+  type: "input",
+  name: "Questions?",
+  message:"Please input your email address",
+},
+
+
+
+
+  ])
+  .then(answers => {
+
+    
+
+
+
+   const tempVar = `
+
+   [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
+
+
+   # Title
+   ${answers.github}
+
+   ## ${"Table of Contents"}
+
+   
+
+
+   [Installation](#Installation)
+    <br>
+   [Usage](#Usage)
+   <br>
+   [Conbtribution](#Contribution)
+   <br>
+   [Testing](#Testing)
+
+
+  ## Installation
+  ${answers.Installation}
+
+  ## Usage Information
+  ${answers.Usage}
+
+  ## Contribution Information
+  ${answers.Contribution}
+
+  ## Testing Information
+  ${answers.Test}
+
+   ## Questions
+
+   Please feel free to reach out with any additional questions through my email or on github.
+   <br>
+   https://github.com/rjmurphy1922
+   <br>
+  ${answers.Email}
+
+   `
+   fs.writeFileSync("README.md", tempVar)
+    console.log(answers.github)
+  })
+  .catch(error => {
+    if(error) {
+   throw error
+    };
+
+   
+
+  })
+
+
+
+}
+
+
+userPrompt();
